b5c63f47ea87eb53d687e83c447a164850e2c7e0
WaltXon <walt.nixon@gmail.com>
2014-10-15 16:13:03 -0500

    rigSchedule function added...first draft

--- createrigschedule.py
+++ createrigschedule.py
@@ -10,20 +10,15 @@ import pandas as pd
 
 
 
-def createRigSchedule(start_date='2014-10-14', contractLenMonths=12, spudToSpudDays = 22): 
-    stime = datetime.datetime.strptime(start_date,'%Y-%m-%d')  
-    etime = stime + du.relativedelta.relativedelta(months = contractLenMonths)
-    diff = du.relativedelta.relativedelta(stime, etime)
-    totaldays = abs(diff.years*365 + diff.months*30 + diff.days)
-    periods = totaldays/spudToSpudDays     
-    rng = pd.date_range(start_date, periods, freq='D')    
-    print stime, etime, totaldays, periods
-    return rng  
-    
-drldays = 22.0
-cterm = 12.0
-dpm = 365.0/12.0
-start_date='2014-10-14'
-periods = (cterm*dpm) / drldays
-
-dr1 = pd.date_range(start_date)
+def rigSchedule(start_date='2014-10-14', cterm=12, drldays = 22): 
+    dpm = 365.0/12.0
+    periods = int((cterm*dpm) / drldays)
+    sdate = datetime.datetime.strptime(start_date,'%Y-%m-%d')  
+    dates = {}
+    d = sdate
+    dates[d] = 'empty'
+    for x in range(0,periods):
+        d =  d + datetime.timedelta(days=drldays)
+        dates[d] = 'empty'
+    print dates
+    return dates
