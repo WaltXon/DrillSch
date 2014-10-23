52101262938cdd71ae2aab816b06e32b29404de2
WaltXon <walt.nixon@gmail.com>
2014-10-23 11:18:48 -0500

    working on refinement func

--- rigwellmatch.py
+++ rigwellmatch.py
@@ -40,6 +40,7 @@ def getEmpty(sch):
 
 def setOOB(wells):
     wellsOutOfBounds =  [w for w in wells.index if wells.get_value(w, 'pref') == []]
+    #print('wellsOutOfBounds = {0}'.format(wellsOutOfBounds))    
     for oob in wellsOutOfBounds: 
         wells.set_value(oob, 'scheduled', 'OOB')
     return wellsOutOfBounds
@@ -50,7 +51,21 @@ def getOOB(wells):
     return [w for w in wells.index if wells.get_value(w, 'scheduled') == 'OOB']
 def getNotSchd(wells):
     return [w for w in wells.index if wells.get_value(w, 'scheduled') == 'no']
-        
+def swap(swapee, empt, swap_well, swappos):
+    #move the swap well to the empty slot
+    schdf['well'][empt] = welldf['name'][swapee]
+    schdf['well_id'][empt] = swapee
+    #move the new well into the now open spot formerly occupied by the swap well
+    schdf['well'][swappos] = welldf['name'][swap_well]
+    schdf['well_id'][swappos] = swap_well
+    #update the welldf table to show the new positions
+    welldf['sch_id'][swap_well] = swappos
+    welldf['scheduled'][swap_well] = 'yes' 
+    welldf['sch_id'][swapee] = empt
+    print("swap well {0} ({1}) moved from position {2} to empty position {3}".format(welldf['name'][swapee], swapee, swappos, empt))    
+    print("new well {0} ({1}) moved to position {2}".format(welldf['name'][swap_well], swap_well, swappos))
+    
+    
 def greedyMatching(wells, sch):
     
     wellsOutOfBounds = getOOB(wells)
@@ -79,36 +94,54 @@ def greedyMatching(wells, sch):
     report(wells)
     return(wells, wellsScheduled, wellsOutOfBounds, wellsToSchedule)
     
-def refineMatch(wells, sch):
+
+def refineMatchInner(wells, sch, prefs, wid):
+    for pos in prefs:
+        print('pos = {0}'.format(pos))
+        #get the id of the well in the first pref spot
+        swapee = sch['well_id'][pos]
+        print('swapee = {0}'.format(swapee))
+        #get a list of the empty spots
+        for empt in getEmpty(sch):
+            print('empt = {0}'.format(empt))
+            #check to see if the well currently occuping the swap spot could legaly be moved to
+            #an empty spot
+            if checkExp(swapee, empt) == 'GOOD':
+                #swap the existing well into the empty spot and the new well into the swap spot                    
+                swap(swapee, empt, wid, pos)
+                return
+
+def refineMatchOuter(wells, sch):
     wellsToSchedule = [w for w in wells.index if wells.get_value(w, 'scheduled') == 'no']
-    res = welldf.iloc[wellsToSchedule].sort(columns='rank', axis=0, ascending=True) #TRUE OR FALSE
+    res = welldf.iloc[wellsToSchedule].sort(columns='rank', axis=0, ascending=False) #TRUE OR FALSE
+    print('')
+    print('--------')    
+    print wells
+    print('--------')    
+    print sch    
+    print('--------')
     print res
+    print('--------')
+    #step through the list of wells that are not scheduled
     for wid in res.index:
-        print('')
+        print('---')
         print('wid = {0}'.format(wid))
+        print('---')
+        #get a list of thier possible rig spots
         prefs = res.get_value(wid, 'pref')
-        for pos in prefs:
-            print('pos = {0}'.format(pos))
-            swapee = sch.get_value(pos, 'well_id')
-            print('swapee = {0}'.format(swapee))
-            for empt in getEmpty(sch):
-                print('empt = {0}'.format(empt))
-                if checkExp(swapee, empt) == 'GOOD':
-                    sch.set_value(empt, 'well', sch.get_value(swapee, 'well'))
-                    sch.set_value(empt, 'well_id', swapee)
-                    sch.set_value(pos, 'well', sch.get_value(wid, 'well'))
-                    sch.set_value(pos, 'well_id', wid)
-                    wells.set_value(wid, 'sch_id', pos)
-                    wells.set_value(wid, 'scheduled', 'yes')
-                    wells.set_value(swapee, 'sch_id', empt)
-                    print("well {0} moved to position {1}".format(swapee, empt))
-                    print("well {0} moved to position {1}".format(wid, pos))
-                    break
-                else:
-                    continue#print("well {0} cannot be moved to position {1}".format(wid, pos))
-        
-    report(wells)
+        #start from the last position and move forward
+        prefs.reverse()
+        #step through possible preferred rig spots
+        refineMatchInner(wells, sch, prefs, wid)
+
+
     
+    report(wells)
+
+def finalMatch(wells, sch):
+    pass
+
+
 def report(df):
     wellsSch= getSchd(df)
     wellsOOB= getOOB(df)
@@ -144,17 +177,22 @@ def report(df):
     print(woSum)
     print('=========================================')
     
-setOOB(welldf)
+
 setPrefs(welldf, schdf)
 rankWells(welldf)
+setOOB(welldf)
 
-gm = greedyMatching(welldf, schdf)
-if len(checkEmpty(schdf)) > 0:
-    
+greedyMatching(welldf, schdf)
+if len(getEmpty(schdf)) > 0:
+    print('')
     print "additional scheduling needed..."
-    refineMatch(welldf, schdf)
-    
-
+    refineMatchOuter(welldf, schdf)
+welldf['pref'].astype(str)
+scht = schdf[['well_id','spud']]
+merged = pd.merge(welldf, scht, left_index=True, right_on='well_id', how='left' ) #, axis=1, join='inner', join_axes=[welldf.index, schridx.index])
+#merged.to_excel(r'C:\Users\WaltN\Desktop\GitHub\DrillSch\result.xlsx')
+mgsort =  merged.sort(columns='spud', ascending=True)
+print mgsort
 #run a greedy matching
 #check to see if there are empty positions on the schedule
 #if so, check to see if the unscheduled wells will fit in the empty slot
